38ca38293f2a7adb6ec1b5754b979dd18ebe9391
Natali-Kremneva <van2gog6@gmail.com>
2022-04-28 22:14:48 +0300

    Fixes in the Utils class

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -8,23 +8,11 @@ public class Util {
     // реализуйте настройку соеденения с БД
     public static Connection getMySQLConnection()
             throws ClassNotFoundException, SQLException {
-        String hostName = "localhost";
-        String dbName = "new_schema";
+        String connectionURL = "jdbc:mysql://localhost:3306/new_schema";
         String userName = "root";
         String password = "mysql";
-        return getMySQLConnection(hostName, dbName, userName, password);
-    }
-
-    public static Connection getMySQLConnection(String hostName, String dbName,
-                                                String userName, String password) throws SQLException,
-            ClassNotFoundException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-
-
-        String connectionURL = "jdbc:mysql://" + hostName + ":3306/" + dbName;
-
-        Connection conn = DriverManager.getConnection(connectionURL, userName,
+        Connection connection = DriverManager.getConnection(connectionURL, userName,
                 password);
-        return conn;
+        return connection;
     }
 }
